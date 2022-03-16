3ad8750e936d4c1939a7a84e604f881df4b64e27
Chuong Tran <chuongninolxag@gmail.com>
2022-03-16 14:32:30 +0700

    config public file

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
-
+const path = require("path");
 const port = 3004;
 
 app.use(morgan("combined"));
+
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
 app.get("/tin-tuc", (req, res) => {
-  res.send("Tin tuc!");
+  // res.send("Tin tuc!");
+  res.setHeader("Content-Type", "application/json");
+  res.send(JSON.stringify({ a: 1, b: 2 }));
 });
 
 app.listen(port, () => {
