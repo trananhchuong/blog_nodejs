60bc5dc0afe448d5097fa579a25437adeca969e6
Chuong Tran <chuongninolxag@gmail.com>
2022-03-16 15:06:12 +0700

    config handlebars

--- src/index.js
+++ src/index.js
@@ -2,14 +2,26 @@ const express = require("express");
 const app = express();
 const morgan = require("morgan");
 const path = require("path");
+const handlebars = require("express-handlebars");
 const port = 3004;
 
 app.use(morgan("combined"));
 
+// Register `hbs.engine` with the Express app.
+app.engine("hbs", handlebars.engine({
+  extname: "hbs"
+}));
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "resources/view"));
+
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.render("home");
+});
+
+app.get("/news", (req, res) => {
+  res.render("news");
 });
 
 app.get("/tin-tuc", (req, res) => {
